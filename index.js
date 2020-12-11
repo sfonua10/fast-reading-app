3461565eea5bfc18e163fae56cd5d4af3d10c61c
Saia Fonua <saiafonua@users-MacBook-Pro.local>
2020-12-11 09:53:49 -0700

    initial styles and skip sup tag

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-// const arrOfWords = ["hi", "how", "are", "you", "doing?"];
+let supTag = document.querySelector("sup");
 function get_text(el) {
   ret = "";
-  var length = el.childNodes.length;
-  for (var i = 0; i < length; i++) {
-    var node = el.childNodes[i];
-    if (node.nodeType != 8) {
-      ret += node.nodeType != 1 ? node.nodeValue : get_text(node);
+  const length = el.childNodes.length;
+  for (let i = 0; i < length; i++) {
+    const node = el.childNodes[i];
+    if (!supTag.isEqualNode(node)) {
+      if (node.nodeType != 8) {
+        ret += node.nodeType != 1 ? node.nodeValue : get_text(node);
+      }
     }
   }
   return ret;
 }
-var words = get_text(document.querySelector("html"));
-var arrOfWords = words.split(" ").filter((word) => word !== "" && word !== "↵");
-console.log(arrOfWords);
 
+const words = get_text(document.querySelector("body"));
+const arrOfWords = words
+  .split(" ")
+  .filter((word) => word !== "" && word !== "↵");
+
+console.log("arrOfWords", arrOfWords);
 for (let i = 0; i < arrOfWords.length; i++) {
   ((i) => {
     setTimeout(() => {
       document.querySelector(
         "#target"
-      ).innerHTML = `<span style="color:blue">${arrOfWords[i]}</span>`;
+      ).innerHTML = `<span style="color:#414141">${arrOfWords[i]}</span>`;
     }, 200 * (i + 1));
   })(i);
 }
