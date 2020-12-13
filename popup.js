ba1f51e035a34460711bde55396c122cde0636cf
Saia Fonua <saiafonua@users-MacBook-Pro.local>
2020-12-13 00:03:19 -0700

    dynamically creates tags

--- popup.js
+++ popup.js
@@ -1,28 +1,40 @@
-let supTag = document.querySelector("sup").tagName;
-let scriptTag = document.querySelector("script").tagName;
+let h1 = document.createElement("H1");
+h1.id = "target";
+h1.style.textAlign = "center";
+h1.style.fontSize = "xx-large";
+h1.style.minHeight = "50px";
+document.body.prepend(h1);
+let button = document.createElement("button");
+button.setAttribute("onclick", "runScript()");
+let t = document.createTextNode("RUN IT MAN");
+button.appendChild(t);
+document.body.prepend(button);
+
+let supTag = document.querySelector("sup")?.tagName;
+let scriptTag = document.querySelector("script")?.tagName;
+
 function get_text(el) {
   ret = "";
-  const length = el.childNodes.length;
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < el.childNodes.length; i++) {
     const node = el.childNodes[i];
-    if (supTag) {
-      supTag !== node.tagName
+    if (supTag || scriptTag) {
+      supTag !== node.tagName && scriptTag !== node.tagName
         ? (ret += node.nodeType != 1 ? node.nodeValue : get_text(node))
         : "";
     }
-    if (scriptTag !== node.tagName) {
-      if (node.nodeType != 8) {
-        ret += node.nodeType != 1 ? node.nodeValue : get_text(node);
-      }
-    }
+    // if (scriptTag !== node.tagName) {
+    //   if (node.nodeType != 8) {
+    //     ret += node.nodeType != 1 ? node.nodeValue : get_text(node);
+    //   }
+    // }
   }
   return ret;
 }
 function runScript() {
-  document.querySelector("button").remove();
+  const regex = /\s|[.]|—|;/g;
   const words = get_text(document.querySelector("body"));
   const arrOfWords = words
-    .split(" ")
+    .split(regex)
     .filter((word) => word !== "" && word !== "↵");
   console.log("arrOfWords", arrOfWords);
   for (let i = 0; i < arrOfWords.length; i++) {
